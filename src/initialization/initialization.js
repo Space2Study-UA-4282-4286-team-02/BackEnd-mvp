4c6b98f4c538cfd00d555fdb1f5beb311556001c
TanyaMykhaylyuk <tanyamykhaylyuk27@gmail.com>
2025-12-18 09:14:07 +0200

    Feature/37/add swagger
    What I did
    
    Installed swagger-ui-express, swagger-jsdoc, js-yaml (added to package.json)
    Added docs/swagger.yaml with Offer & Message routes
    Added swagger initialization to src/initialization/initialization.js

--- src/initialization/initialization.js
+++ src/initialization/initialization.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJSDoc = require('swagger-jsdoc')
 
 const {
   config: { CLIENT_URL }
@@ -21,6 +26,45 @@ const initialization = (app) => {
       allowedHeaders: 'Content-Type, Authorization'
     })
   )
+
+  const docsPath = path.join(__dirname, '../../docs/swagger.yaml')
+  let swaggerDocument = null
+
+  try {
+    if (fs.existsSync(docsPath)) {
+      const yamlText = fs.readFileSync(docsPath, 'utf8')
+      swaggerDocument = yaml.load(yamlText)
+    }
+  } catch (err) {}
+ 
+  if (!swaggerDocument) {
+    const swaggerOptions = {
+      definition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'BackEnd-mvp API',
+          version: '1.0.0',
+          description: 'Automatically generated API docs (swagger-jsdoc)'
+        },
+        servers: [
+          {
+            url: `http://localhost:${process.env.SERVER_PORT || 8080}`
+          }
+        ]
+      },
+      apis: ['./src/routes/*.js', './src/controllers/*.js']
+    }
+
+    swaggerDocument = swaggerJSDoc(swaggerOptions)
+    app.set('swaggerJSDocOptions', swaggerOptions)
+  }
+
+  app.set('swaggerDoc', swaggerDocument)
+  app.set('swaggerOptions', { explorer: true })
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, app.get('swaggerOptions')))
+  }
 
   app.use('/', router)
 
