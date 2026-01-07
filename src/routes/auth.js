34d926f2f956a940d76cd957ce181b1c16ef2a75
TanyaMykhaylyuk <tanyamykhaylyuk27@gmail.com>
2026-01-07 22:20:40 +0100

    fix: resolve category routes conflict; fix auth asyncWrapper next handling and google-auth handler (#22)
    resolve category routes conflict; fix auth asyncWrapper next handling and google-auth handler

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -18,7 +18,7 @@ router.post(
 )
 router.post('/login', validationMiddleware(loginValidationSchema), asyncWrapper(authController.login))
 
-router.post('/google-auth', asyncWrapper(authController.googleAuthHandler))
+router.post('/google-auth', asyncWrapper(authController.googleLogin))
 
 router.post('/logout', asyncWrapper(authController.logout))
 router.get('/refresh', asyncWrapper(authController.refreshAccessToken))
