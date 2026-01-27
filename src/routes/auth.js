24384e3a3e9d7b29d2fc3f9942eccc5c484f7815
Valentyna Hanich <166041128+vhanich@users.noreply.github.com>
2026-01-27 22:11:22 +0100

    Revert "#74-Add getMe endpoint to retrieve current user data" (#43)
    * Revert "Revert "#74-Add getMe endpoint to retrieve current user data" (#42)"
    
    This reverts commit 1abed15250a99378a9267fd4c3d96061e87a014f.
    
    * Add GET route for fetching user details

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -19,10 +19,8 @@ router.post(
 router.post('/login', validationMiddleware(loginValidationSchema), asyncWrapper(authController.login))
 
 router.post('/google-auth', asyncWrapper(authController.googleAuthHandler))
-
-router.post('/logout', asyncWrapper(authController.logout))
-
 router.get('/me', authMiddleware, asyncWrapper(authController.getMe))
+router.post('/logout', asyncWrapper(authController.logout))
 router.get('/refresh', asyncWrapper(authController.refreshAccessToken))
 router.get('/confirm-email/:token', asyncWrapper(authController.confirmEmail))
 router.post(
