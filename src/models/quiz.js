fe92cd031b957cb1b79bdb46cee2c610df29269f
TanyaMykhaylyuk <tanyamykhaylyuk27@gmail.com>
2026-02-05 08:36:09 +0200

    Create "Quiz" model (#129) (#38)
    Create "Quiz" model

--- src/models/quiz.js
+++ src/models/quiz.js
@@ -3,76 +3,79 @@ const { Schema, model } = require('mongoose')
 const {
   enums: { QUIZ_VIEW_ENUM, RESOURCES_TYPES_ENUM }
 } = require('~/consts/validation')
-const { QUIZ, USER, RESOURCES_CATEGORY, QUESTION } = require('~/consts/models')
-const {
-  FIELD_CANNOT_BE_EMPTY,
-  FIELD_CANNOT_BE_LONGER,
-  FIELD_CANNOT_BE_SHORTER,
-  ENUM_CAN_BE_ONE_OF
-} = require('~/consts/errors')
+const {   QUIZ,
+          QUESTION,
+          USER,
+          RESOURCES_CATEGORY
+       } = require('~/consts/models')
+const {     FIELD_CANNOT_BE_EMPTY,
+            FIELD_CANNOT_BE_SHORTER,
+            FIELD_CANNOT_BE_LONGER,
+            ENUM_CAN_BE_ONE_OF
+       } = require('~/consts/errors')
 
 const quizSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: [true, FIELD_CANNOT_BE_EMPTY('title')],
-      minLength: [1, FIELD_CANNOT_BE_SHORTER('title', 1)],
-      maxLength: [100, FIELD_CANNOT_BE_LONGER('title', 100)]
-    },
-    description: {
-      type: String,
-      required: [true, FIELD_CANNOT_BE_EMPTY('description')],
-      minLength: [1, FIELD_CANNOT_BE_SHORTER('description', 1)],
-      maxLength: [1000, FIELD_CANNOT_BE_LONGER('description', 1000)]
-    },
-    items: {
-      type: [{ type: Schema.Types.ObjectId, ref: QUESTION }],
-      default: []
-    },
-    category: {
-      type: Schema.Types.ObjectId,
-      ref: RESOURCES_CATEGORY,
-      default: null
-    },
-    author: {
-      type: Schema.Types.ObjectId,
-      ref: USER,
-      required: [true, FIELD_CANNOT_BE_EMPTY('author')]
-    },
-    quizView: {
-      type: String,
-      enum: {
-        values: QUIZ_VIEW_ENUM,
-        message: ENUM_CAN_BE_ONE_OF('quiz view', QUIZ_VIEW_ENUM)
-      },
-      default: 'Scroll'
-    },
-    shuffleQuestions: {
-      type: Boolean,
-      default: false
-    },
-    pointValues: {
-      type: Boolean,
-      default: false
-    },
-    scoredUnscoredResponses: {
-      type: Boolean,
-      default: false
-    },
-    correctAnswers: {
-      type: Boolean,
-      default: false
-    },
-    resourceType: {
+{
+ title:{
+    type: String,
+    required: [true, FIELD_CANNOT_BE_EMPTY('title')],
+    minlength: [1, FIELD_CANNOT_BE_SHORTER('title',1)],
+    maxlength: [100, FIELD_CANNOT_BE_LONGER('title',100)]
+ },
+ description:{
+     type: String,
+     default: '',
+     maxlength: [150, FIELD_CANNOT_BE_LONGER('description',150)]
+ },
+ items:{
+    type: [{type: Schema.Types.ObjectId, ref : QUESTION}],
+    required: [true, FIELD_CANNOT_BE_EMPTY('items')],
+    validate: {
+        validator: (v) => Array.isArray(v) && v.length > 0,
+        message: FIELD_CANNOT_BE_EMPTY('items')
+      }
+ },
+ author:{
+    type: Schema.Types.ObjectId,
+    ref: USER,
+    required: [true, FIELD_CANNOT_BE_EMPTY('author')]
+ },
+ category:{
+    type: Schema.Types.ObjectId,
+    ref: RESOURCES_CATEGORY,
+    default: null
+},
+ resourceType: {
       type: String,
       enum: {
         values: RESOURCES_TYPES_ENUM,
-        message: ENUM_CAN_BE_ONE_OF('resource type', RESOURCES_TYPES_ENUM)
+        message: ENUM_CAN_BE_ONE_OF('resourceType', RESOURCES_TYPES_ENUM)
       },
-      default: 'quizzes'
+      default: 'quizzes' 
+    },
+ settings: {
+  quizView: {
+        type: String,
+        enum: {
+          values: QUIZ_VIEW_ENUM,
+          message: ENUM_CAN_BE_ONE_OF('quiz view', QUIZ_VIEW_ENUM)
+        },
+        default: 'Scroll'
+      },
+      scoredUnscoredResponses: { type: Boolean, default: false },
+      showCorrectAnswers: { type: Boolean, default: false },
+      shuffleQuestions: { type: Boolean, default: false },
+      pointValues: { type: Boolean, default: false }
     }
   },
-  { timestamps: true, versionKey: false }
+{  
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    versionKey: false,
+    id: false
+  }
+
 )
 
 module.exports = model(QUIZ, quizSchema)
